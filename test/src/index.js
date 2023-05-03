c7ef9356ea325943c28531ab52cfb481eeb8fa5d
Blake Hansen <b.hansen9264@gmail.com>
2023-05-03 16:57:43 -0700

    File structure fixed to common standard  Files added __test__ , actions , styles

--- test/src/index.js
+++
@@ -1,9 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import React from 'react';
-// import App from '../../client/src/components/App.jsx';
-
-describe('app', () => {
-  it('should work', () => {
-    expect(true).toBe(true);
-  });
-});
