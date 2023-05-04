ffae682de4858e5c8b6a62af0ca35248ed2be5ca
Noah Beito <noah.beito@gmail.com>
2023-05-04 11:51:43 -0700

    adding skeleton for main components

---
+++ client/src/RelatedItemsComparison/components/RelatedItems.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function RelatedItems() {
+  return (
+    <div className="related-items-container">
+      <h3>Related Items</h3>
+    </div>
+  );
+}
---
+++ client/src/RelatedItemsComparison/components/YourOutfit.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function YourOutfit() {
+  return (
+    <div className="your-outfit-container">
+      <h3>Your Outfit</h3>
+    </div>
+  );
+}
