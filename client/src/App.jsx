0306d4f2b8f018faecc114b393960900f998bba9
Thang Nguyen <Thang14nguyen@gmail.com>
2023-05-05 09:11:17 -0700

    enabled RR in App.jsx, added partial skeleton for RR

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
       {/* <Overview /> */}
       <RelatedItemsComparisons />
       <QuestionsAnswers />
-      {/* <RatingsReviews /> */}
+      <RatingsReviews />
     </div>
   );
 }
---
+++ client/src/RatingsReviews/components/ReviewsList/IndividualReviewTile/IndividualReviewTile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NameDate from '../../../../components/NameDate';
+
+export default function ReviewTile() {
+
+  return (
+    <div>
+      <NameDate />
+    </div>
+  )
+}
