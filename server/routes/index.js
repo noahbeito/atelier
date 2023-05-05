212d47e79e90c47089c87f40edfa894589ebfd4d
Noah Beito <noah.beito@gmail.com>
2023-05-05 10:24:39 -0700

    imported related products route to routes index file and passed app to the route.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const questionsAnswers = require('./questionsAnswers');
 const products = require('./products');
+const related = require('./related');
 
 module.exports = (app) => {
   questionsAnswers(app);
   products(app);
+  related(app);
 };
