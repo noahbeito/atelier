70da08e73eb87ae933474549c8d3b668671c898d
Thomas Spradling <thomasspradling13@gmail.com>
2023-05-06 15:18:19 -0700

    Served static assets as well

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use('/assets', express.static(path.join(__dirname, '../client/assets')));
 
 // Set up routes
 routes(app);
