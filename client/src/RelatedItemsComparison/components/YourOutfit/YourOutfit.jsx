bb898891697f0789beb53c55e63bf6102e226601
Noah Beito <noah.beito@gmail.com>
2023-05-08 12:22:43 -0700

    updated your outfit to save current outfit to local storage so that it persists through page reloads

--- client/src/RelatedItemsComparison/components/YourOutfit/YourOutfit.jsx
+++ client/src/RelatedItemsComparison/components/YourOutfit/YourOutfit.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AddItemToOutfit from './AddItemToOutfit';
 import ProductCard from '../ProductCard/ProductCard';
 import { Title, Carousel } from '../../styles';
 
 export default function YourOutfit() {
   const [outfit, setOutfit] = useState([]);
+
+  useEffect(() => {
+    const savedOutfit = JSON.parse(localStorage.getItem('outfit'));
+    if (savedOutfit) {
+      setOutfit(savedOutfit);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('outfit', JSON.stringify(outfit));
+  }, [outfit]);
 
   const AddToOutfitClickHandler = (productId) => {
     if (!outfit.includes(productId)) {
