84019a18e457f6ce11698f8be1f260e9aa61d0e6
Blake Hansen <b.hansen9264@gmail.com>
2023-05-09 10:55:51 -0700

    Adding beginning of reducer build

--- client/src/Overview/index.jsx
+++ client/src/Overview/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
+// import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import ProductDisplay from './components/ProductDisplay/ProductDisplay';
 import ProductInfo from './components/ProductInfo/ProductInfo';
@@ -14,8 +15,25 @@ const Section = styled.section`
 `;
 export default function Overview() {
   const [renderCheckout, setRenderCheckout] = useState(true);
+  // const isLoading = useSelector((state) => state.product.isLoading
+  //                                       || state.questionsAnswers.main.loading);
+  // const productId = useSelector((state) => state.product.data.id);
+  // const productStyles = useSelector((state) => state.product.data.id.styles);
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   if (productId) {
+  //     dispatch({ type: '@styles/FETCH_DATA' });
+  //     axios.get('/products/40344/styles', { params: { product_id: productId } })
+  //       .then((result) => {
+  //         dispatch({ type: '@styles/SET_DATA', payload: result.data.styles });
+  //       })
+  //       .catch((error) => {
+  //         dispatch({ type: '@styles/FETCH_FAILED', payload: error });
+  //       });
+  //   }
+  // }, [productId]);
   const onClick = useCallback(() => {
-    console.log('I have been clicked!');
     setRenderCheckout(!renderCheckout);
   }, [renderCheckout]);
   return (
