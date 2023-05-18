548cbbcf2b9b17ff0ed16a8ca2969acb9acf9ab1
Noah Beito <noah.beito@gmail.com>
2023-05-18 16:08:39 -0700

    changed title alignment

--- client/src/RelatedItemsComparison/styles/index.js
+++ client/src/RelatedItemsComparison/styles/index.js
@@ -25,7 +25,7 @@ const OutfitContainer = styled(Container)`
 `;
 
 const Title = styled.h2`
-  grid-area: 1 / 2 / span 1 / span 1;
+  grid-area: 1 / 1 / span 1 / span 1;
   min-width: 200px;
   margin-inline: auto;
   margin-top: 1.5rem;
