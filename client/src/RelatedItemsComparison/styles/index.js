6f19d2be1c804fd99da3f589b3bcb0df27c0a1f7
Noah Beito <noah.beito@gmail.com>
2023-05-18 18:00:04 -0700

    updated alignment of right chevron

--- client/src/RelatedItemsComparison/styles/index.js
+++ client/src/RelatedItemsComparison/styles/index.js
@@ -122,6 +122,7 @@ const ChevronButton = styled.button`
 const StyledRightButton = styled(ChevronButton)`
   grid-area: 2 / 4 / span 1 / span 1;
   justify-self: end;
+  margin-right: 0.5rem;
 `;
 
 const StyledLeftButton = styled(ChevronButton)`
